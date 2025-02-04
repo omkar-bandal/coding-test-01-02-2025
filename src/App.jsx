df65d64d2e3002a84b99992da25913e51c3eac00
omkar-bandal <omkarbandal2010@gmail.com>
2025-02-04 16:31:08 +0530

    coding test

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,7 @@ const App = () => {
             <Route path="/" element={<Login />} />
             <Route path="/Regitration" element={<Regitration />} />
             <Route path="/Dashboard" element={<Dashboard />} />
-          </Routes>
-
-         
+          </Routes>      
         </div>
       </Router>
     </div>
